b7e4215180b53f0618887d7f3d4a21646d394629
Eric C <39015447+ricetech@users.noreply.github.com>
2021-08-21 04:26:32 -0400

    Add status change event test code

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,15 @@ import { Client, Intents } from "discord.js";
 dotenv.config();
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES],
 });
 
 client.once("ready", () => {
   console.log("Ready!");
 });
+
+client.on("presenceUpdate", (oldPresence, newPresence) => {
+  console.log("Presence update for " + newPresence.user?.tag + ": " + newPresence.status);
+})
 
 client.login(process.env.DISCORD_TOKEN);
