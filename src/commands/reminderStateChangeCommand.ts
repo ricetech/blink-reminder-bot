4c039e9e099dcf03e9fd4ae8e12b31cb9e1ea36b
Eric C <39015447+ricetech@users.noreply.github.com>
2021-08-30 13:14:42 -0400

    Implement reminderStateChangeCommand

--- src/commands/reminderStateChangeCommand.ts
+++ src/commands/reminderStateChangeCommand.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
+import { UserConfigModel } from "../db";
 
 const reminderStateChangeCommand = {
   data: new SlashCommandBuilder()
@@ -14,7 +15,44 @@ const reminderStateChangeCommand = {
         .addChoice("off", "off")
     ),
   async execute(interaction: CommandInteraction) {
-    await interaction.reply("Command 'reminders' not implemented yet!");
+    const uid = interaction.user.id;
+    const state = interaction.options.getString("state", true);
+    const enabled: boolean = state == "on";
+
+    const userConfig = await UserConfigModel.findOne({ where: { uid: uid } });
+
+    if (userConfig) {
+      const affectedRows = await UserConfigModel.update(
+        { enabled: enabled },
+        { where: { uid: uid } }
+      );
+      if (affectedRows[0] > 0) {
+        if (enabled == userConfig.get("enabled")) {
+          await interaction.reply(
+            `Your blink reminders were already ${state}. Nothing was changed.`
+          );
+        } else {
+          await interaction.reply(
+            `Your blink reminders are now turned ${state}.`
+          );
+        }
+      }
+    } else {
+      try {
+        await UserConfigModel.create({
+          uid: uid,
+          enabled: enabled,
+          interval: 20,
+        });
+        await interaction.reply(
+          `Your blink reminders are now turned ${state}.`
+        );
+      } catch (e) {
+        return interaction.reply(
+          "Something went wrong with creating your profile."
+        );
+      }
+    }
   },
 };
 
